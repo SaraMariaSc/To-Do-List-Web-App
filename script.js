9e986deb5506b5cae1c033bfc5096f9a1423fba4
SaraMariaSc <sara.schitelea@gmail.com>
2020-10-04 14:07:55 +0300

    fixed everything

--- script.js
+++ script.js
@@ -4,10 +4,28 @@ var ul = document.querySelector("ul");
 var li = document.querySelector("li");
 var done = document.querySelectorAll(".done");
 var deleteButton = document.querySelectorAll(".delete");
+var randomImageArray = ["no-connection-6.png", "taxi-satch-virus-molecules.png", "waiting-6.png",
+                        "taxi-uploading.png", "taxi-teamsport.png", "taxi.png",
+                        "taxi-healthcare.png", "taxi-design.png", "taxi-610.png",
+                        "taxi-594.png", "taxi-594.png", "taxi-564.png",
+                        "taxi-6.png", "taxi-4.png", "taxi-2.png"];
+var randomImageDiv = document.getElementById("random-image");
+
+function changeRandomImage(imageAray){
+    var num = Math.floor(Math.random() * imageAray.length);
+    var img = imageAray[num];
+    var imgHtml = "<img src=\"./img/" 
+                 + img 
+                 + "\" id=\"random-image\">";
+    console.log(imgHtml);
+    randomImageDiv.innerHTML = imgHtml;
+}
+
+changeRandomImage(randomImageArray);
 
 //ADD NEW ITEMS
 function inputLength(){
-    return input.value.length
+    return input.value.length;
 }
 
 function createListElement(){
@@ -16,26 +34,21 @@ function createListElement(){
     removeDone(p);
     p.innerHTML = input.value;
 
-    var deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("delete");    
-    deleteBtn.classList.add("button-spacing");    
-    deleteBtn.innerHTML = "Delete";
+    // create delete icon button
+    var deleteIcon = document.createElement("img");
+    deleteIcon.classList.add("delete");    
+    deleteIcon.classList.add("delete-icon");    
+    deleteIcon.src = "img/delete.png";
+    deleteIcon.alt = "delete-icon";
 
     var liNew = document.createElement("li");
-    liNew.appendChild(deleteBtn);
+    liNew.appendChild(deleteIcon);
     liNew.appendChild(p);
     ul.appendChild(liNew);
     input.value="";
 
-    deleteBtn.addEventListener("click",event => {deleteItem(deleteBtn);})
+    deleteIcon.addEventListener("click",event => {deleteItem(deleteIcon);})
     p.addEventListener("click", event => {toggleDone(p);})
-}
-
-function addListAfterClick(){
-    if(inputLength() > 0)
-    { 
-        createListElement();
-    }
 }
 
 function addListAfterEnter(event){
@@ -45,7 +58,6 @@ function addListAfterEnter(event){
     }
 }
 
-button.addEventListener("click", addListAfterClick );
 input.addEventListener("keypress", addListAfterEnter );
 
 //TOGGLE DONE 
